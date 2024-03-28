e5168674de2ce1b14076e7a31b9aa20480e8d546
David Szankovszky <szankovszky.david@gmail.com>
2024-03-28 16:08:25 +0100

    Full CI/CD

--- public/client.js
+++ public/client.js
@@ -4,8 +4,8 @@ const increaseButton = document.getElementById("increaseButton");
 const decreaseButton = document.getElementById("decreaseButton");
 const startNumber = document.getElementById("countNumber");
 
-function loadNumber(){
-    fetch('/getNumber', {method: 'GET'})
+async function loadNumber(){
+    await fetch('/getNumber', {method: 'GET'})
     .then(function(response){
         if(response.ok){
           response.json()
